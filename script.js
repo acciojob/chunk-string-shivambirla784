8a35df96546a943fcc7d43b5f0f3679b7c3b8dbf
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2025-09-02 21:23:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	if(!str || size<=0) return [];
+	const res = [];
+	for(let i=0;i<str.length;i+=size){
+		res.push(str.slice(i,i+size));
+	}
+	return res;
 }
 
-// Do not change the code below
+// // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
